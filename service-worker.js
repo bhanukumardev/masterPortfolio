4e26970e5200ed0933cce52ff984c90c1709d4ed
bhanukumardev <bhanukumardev@users.noreply.github.com>
2025-06-06 16:46:34 +0000

    deploy: 2a97871bde69cd6cae98b3d495fd4a0bd00cfd30

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.65409cc88a4be40c85f819d92271b7dc.js"
+  "/masterPortfolio/precache-manifest.445f6cdfe8cbfc7621700e689beb5b35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/masterPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
